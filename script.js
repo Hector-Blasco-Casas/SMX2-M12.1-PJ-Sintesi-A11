88dccf1e428b5c1dfb056315a4b3c9d83b8f1466
Hector-Blasco-Casas <464424.joan23@fje.edu>
2026-03-27 00:37:54 +0100

    Comentarios añadidos

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
-const audio = document.getElementById('musica-fondo');
+const audio = document.getElementById('musica-fondo'); // Obtenemos el audio que hemos creado en el HTML
 
-audio.volume = 0.4;
+audio.volume = 0.4; // Establecemos el volumen del audio
